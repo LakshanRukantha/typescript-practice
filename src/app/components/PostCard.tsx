d5e752ab21074b9424e6f8831f0e2ec76a7daaff
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-09-30 14:03:26 +0530

    fix: Minor UI Fixing

--- src/app/components/PostCard.tsx
+++ src/app/components/PostCard.tsx
@@ -11,8 +11,8 @@ type PostCardProps = {
 
 const PostCard = (props: PostCardProps) => {
   return (
-    <div className="flex flex-col gap-2 p-2 border-2 transition-all bg-gradient-to-tr hover:shadow-md from-violet-100 rounded-md hover:border-violet-500">
-      <div className="font-semibold hover:underline hover:text-violet-800 text-lg text-slate-800 md:min-h-[55px]">
+    <div className="flex flex-col justify-between gap-2 p-2 border-2 transition-all bg-gradient-to-tr hover:shadow-md from-violet-100 rounded-md hover:border-violet-500">
+      <div className="font-semibold hover:underline hover:text-violet-800 text-lg text-slate-800 text-ellipsis line-clamp-2">
         {props.title}
       </div>
       <div>
