cbf78c56714a5d7ca239aedd13935d05794f5def
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-10-07 18:44:59 +0530

    fix: Production Build Compile Error Fixing Attempt 0001

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,9 +2,9 @@ import { NextAuthOptions } from "next-auth";
 import { githubAuth, googleAuth } from "@/app/utils/AuthOptionProviders";
 import NextAuth from "next-auth";
 
-const handler: NextAuthOptions = NextAuth({
+const handler = NextAuth({
   providers: [googleAuth, githubAuth],
   secret: process.env.AUTH_SECRET as string,
-});
+}) as NextAuthOptions;
 
 export { handler as GET, handler as POST };
