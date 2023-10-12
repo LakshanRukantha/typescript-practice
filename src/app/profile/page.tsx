dc1b175a98d675c8458e854f54d79f9bbb901c9c
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-10-12 22:00:54 +0530

    fix: Minor UI/UX Improvements

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -19,12 +19,12 @@ const Profile = () => {
   };
 
   return session.status === "authenticated" ? (
-    <div className="w-full mx-auto flex flex-col border p-4 gap-4 shadow rounded my-5">
+    <div className="w-full mx-auto flex flex-col border md:p-4 shadow rounded my-5">
       <div className="relative flex max-h-[75px] flex-row rounded-t bg-violet-100 items-start justify-between p-4">
         <div className="relative top-2">
           <Image
             src={session.data?.user?.image as string}
-            className="relative border-8 border-violet-100 outline hover:outline-violet-500 transition-all rounded-full"
+            className="relative border-8 border-violet-100 outline outline-2 hover:outline-violet-500 transition-all rounded-full"
             width={100}
             height={100}
             alt={`profile ${session.data?.user?.name}`}
@@ -34,22 +34,42 @@ const Profile = () => {
             <span className="inline-flex rounded-full h-3 w-3 bg-green-500"></span>
           </div>
         </div>
-        <Button
-          title="Sign Out"
-          priority="secondary"
-          hasAction={true}
-          icon={<HiOutlineLogout className="text-xl" />}
-          taskFunc={handleSignOut}
-        />
+        <div className="h-full my-auto">
+          <Button
+            title="Sign Out"
+            priority="secondary"
+            hasAction={true}
+            icon={<HiOutlineLogout className="text-xl" />}
+            taskFunc={handleSignOut}
+          />
+          {/* <div className={`hidden md:block`}>
+            <Button
+              title="Sign Out"
+              priority="secondary"
+              hasAction={true}
+              icon={<HiOutlineLogout className="text-xl" />}
+              taskFunc={handleSignOut}
+            />
+          </div>
+          <div className={`block md:hidden`}>
+            <Button
+              title=""
+              priority="secondary"
+              hasAction={true}
+              icon={<HiOutlineLogout className="text-xl" />}
+              taskFunc={handleSignOut}
+            />
+          </div> */}
+        </div>
       </div>
-      <div className="pt-10">
+      <div className="pt-16 pb-4 md:pb-0 px-4 md:px-0">
         <h1 className="text-3xl font-bold text-slate-800">
           {session.data?.user?.name}
         </h1>
 
         <p className="text-gray-500 flex items-center gap-1">
-          <HiOutlineMail className="text-xl" />
-          {session.data?.user?.email}
+          <HiOutlineMail className="text-xl mt-[1px]" />
+          <span>{session.data?.user?.email}</span>
         </p>
       </div>
     </div>
