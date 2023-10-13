e16d42f845b527e7765a0579381d92ae8e96f9af
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-10-13 22:21:47 +0530

    fix: Code Refactor By Creating User Profile Card Component

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,79 +1,24 @@
 "use client";
 
-import Button from "@/components/Button";
-import { signOut, useSession } from "next-auth/react";
-import Image from "next/image";
+import ProfileCard from "@/components/ProfileCard";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { HiOutlineLogout, HiOutlineMail } from "react-icons/hi";
 
 const Profile = () => {
   const session = useSession();
   const router = useRouter();
 
-  if (session.status === "unauthenticated") {
+  if (!session || session.status === "unauthenticated") {
     router.push("/signin");
+    return null;
   }
 
-  const handleSignOut = () => {
-    signOut({ callbackUrl: "/" });
-  };
-
   return session.status === "authenticated" ? (
-    <div className="w-full mx-auto flex flex-col border md:p-4 shadow rounded-md my-5">
-      <div className="relative flex max-h-[75px] flex-row rounded-t md:rounded-t bg-violet-100 items-start justify-between p-4">
-        <div className="relative flex items-center justify-center top-2">
-          <Image
-            src={session.data?.user?.image as string}
-            className="relative border-8 border-violet-100 outline outline-2 h-24 w-24 outline-white transition-all rounded-full"
-            width={100}
-            height={100}
-            alt={`profile ${session.data?.user?.name}`}
-          />
-          <span className="absolute animate-spin duration-1000 h-[100px] w-[100px] rounded-full mx-auto border-2 border-transparent hover:border-y-violet-500"></span>
-          <div className="absolute flex items-center justify-center h-4 w-4 top-[6px] right-[6px]">
-            <span className="absolute animate-ping rounded-full h-3 w-3 bg-green-500"></span>
-            <span className="inline-flex rounded-full h-3 w-3 bg-green-500"></span>
-          </div>
-        </div>
-        <div className="h-full my-auto">
-          <Button
-            title="Sign Out"
-            priority="secondary"
-            hasAction={true}
-            icon={<HiOutlineLogout className="text-xl" />}
-            taskFunc={handleSignOut}
-          />
-          {/* <div className={`hidden md:block`}>
-            <Button
-              title="Sign Out"
-              priority="secondary"
-              hasAction={true}
-              icon={<HiOutlineLogout className="text-xl" />}
-              taskFunc={handleSignOut}
-            />
-          </div>
-          <div className={`block md:hidden`}>
-            <Button
-              title=""
-              priority="secondary"
-              hasAction={true}
-              icon={<HiOutlineLogout className="text-xl" />}
-              taskFunc={handleSignOut}
-            />
-          </div> */}
-        </div>
-      </div>
-      <div className="pt-16 pb-4 md:pb-0 px-4 md:px-0">
-        <h1 className="text-3xl font-bold text-slate-800">
-          {session.data?.user?.name}
-        </h1>
-
-        <p className="text-gray-500 flex items-center gap-1">
-          <HiOutlineMail className="text-xl mt-[1px]" />
-          <span>{session.data?.user?.email}</span>
-        </p>
-      </div>
-    </div>
+    <ProfileCard
+      name={session.data.user?.name as string}
+      email={session.data.user?.email as string}
+      image={session.data.user?.image as string}
+    />
   ) : (
     <>{/* Add loading state element here */}</>
   );
