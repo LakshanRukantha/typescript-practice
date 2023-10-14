553e86c04ba6180ed53fa6707fc8ea78a3c5131d
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-10-14 22:00:37 +0530

    fix: Remove Unwanted API Endpoint

--- src/app/api/hello/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export const GET = async (req: NextRequest) => {
-  return NextResponse.json({ method: req.method, req_url: req.nextUrl });
-};
-
-export const POST = async (req: NextRequest) => {
-  return NextResponse.json({ method: req.method, req_url: req.nextUrl });
-};
