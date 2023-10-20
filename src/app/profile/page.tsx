02fc4ba9e1338821a2a0b8d40883eb85bd9c1fbd
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-10-20 15:28:58 +0530

    feat: Develop Profile Content Layout

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import PostCard from "@/components/PostCard";
 import ProfileCard from "@/components/ProfileCard";
+import posts from "@/helpers/posts";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
@@ -14,11 +16,33 @@ const Profile = () => {
   }
 
   return session.status === "authenticated" ? (
-    <ProfileCard
-      name={session.data.user?.name as string}
-      email={session.data.user?.email as string}
-      image={session.data.user?.image as string}
-    />
+    <>
+      <ProfileCard
+        name={session.data.user?.name as string}
+        email={session.data.user?.email as string}
+        image={session.data.user?.image as string}
+      />
+      <div className="relative flex flex-col md:flex-row items-start gap-4 mb-4">
+        <div className="w-full md:w-1/3 bg-violet-100 border-2 transition-all hover:border-violet-500 rounded-md top-20 md:sticky p-4">
+          <h3>Followers ==== 105</h3>
+          <h3>Following ==== 12</h3>
+          <h3>Posts ==== 124</h3>
+          <h3>Profile Views ==== 1K</h3>
+        </div>
+        <div className="w-full flex flex-col gap-4 md:w-2/3">
+          {posts.map((post) => (
+            <PostCard
+              key={post.id}
+              title={post.title}
+              content={post.content}
+              date={post.date}
+              views={post.views}
+              id={post.id}
+            />
+          ))}
+        </div>
+      </div>
+    </>
   ) : (
     <>{/* Add loading state element here */}</>
   );
