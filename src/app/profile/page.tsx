f65cf8ebe880c023c96a0f93fcc32807b2a0fe92
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-10-22 00:16:25 +0530

    feat: Develop User Statistics Card & Minor UI/UX Improvements

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,9 +5,7 @@ import ProfileCard from "@/components/ProfileCard";
 import posts from "@/helpers/posts";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import millify from "millify";
-import { AiOutlineHeart } from "react-icons/ai";
-import { GrArticle } from "react-icons/gr";
+import UserStatisticsCard from "@/components/UserStatisticsCard";
 
 const Profile = () => {
   const session = useSession();
@@ -26,17 +24,8 @@ const Profile = () => {
         email={session.data.user?.email as string}
         image={session.data.user?.image as string}
       />
-      <div className="relative flex flex-col md:flex-row items-start gap-4 mb-4">
-        <div className="flex flex-col gap-4 w-full md:w-1/3 bg-violet-50 border-2 transition-all hover:border-violet-500 rounded-md top-20 md:sticky p-4">
-          <h3 className="flex flex-row flex-nowrap items-center gap-2">
-            <GrArticle className="text-xl mt-[1px]" />
-            <span>{millify(85)} Articles Published</span>
-          </h3>
-          <h3 className="flex flex-row flex-nowrap items-center gap-2">
-            <AiOutlineHeart className="text-xl mt-[1px]" />
-            <span>{millify(25000)} Total Likes</span>
-          </h3>
-        </div>
+      <div className="relative flex flex-col md:justify-between md:flex-row items-start gap-4 mb-4">
+        <UserStatisticsCard />
         <div className="w-full flex flex-col gap-4 md:w-2/3">
           {posts.map((post) => (
             <PostCard
---
+++ src/components/UserStatisticsCard.tsx
@@ -0,0 +1,21 @@
+import millify from "millify";
+import { AiOutlineHeart } from "react-icons/ai";
+import { GrArticle } from "react-icons/gr";
+
+const UserStatisticsCard = () => {
+  return (
+    <div className="flex flex-col gap-4 w-full md:w-1/3 bg-white border-2 shadow rounded-md top-20 md:sticky p-4">
+      <h3 className="flex flex-row flex-nowrap items-center gap-2">
+        <GrArticle className="text-xl mt-[1px]" />
+        <span>{millify(85)} Articles Published</span>
+      </h3>
+      <hr />
+      <h3 className="flex flex-row flex-nowrap items-center gap-2">
+        <AiOutlineHeart className="text-xl mt-[1px]" />
+        <span>{millify(25000)} Total Likes</span>
+      </h3>
+    </div>
+  );
+};
+
+export default UserStatisticsCard;
