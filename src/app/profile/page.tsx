fffdb32dd6dcaa963c11f3668df5402c8cb7010c
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-10-21 23:33:09 +0530

    fix: Update Statistics Section

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,6 +5,9 @@ import ProfileCard from "@/components/ProfileCard";
 import posts from "@/helpers/posts";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import millify from "millify";
+import { AiOutlineHeart } from "react-icons/ai";
+import { GrArticle } from "react-icons/gr";
 
 const Profile = () => {
   const session = useSession();
@@ -24,11 +27,15 @@ const Profile = () => {
         image={session.data.user?.image as string}
       />
       <div className="relative flex flex-col md:flex-row items-start gap-4 mb-4">
-        <div className="w-full md:w-1/3 bg-violet-100 border-2 transition-all hover:border-violet-500 rounded-md top-20 md:sticky p-4">
-          <h3>Followers ==== 105</h3>
-          <h3>Following ==== 12</h3>
-          <h3>Posts ==== 124</h3>
-          <h3>Profile Views ==== 1K</h3>
+        <div className="flex flex-col gap-4 w-full md:w-1/3 bg-violet-50 border-2 transition-all hover:border-violet-500 rounded-md top-20 md:sticky p-4">
+          <h3 className="flex flex-row flex-nowrap items-center gap-2">
+            <GrArticle className="text-xl mt-[1px]" />
+            <span>{millify(85)} Articles Published</span>
+          </h3>
+          <h3 className="flex flex-row flex-nowrap items-center gap-2">
+            <AiOutlineHeart className="text-xl mt-[1px]" />
+            <span>{millify(25000)} Total Likes</span>
+          </h3>
         </div>
         <div className="w-full flex flex-col gap-4 md:w-2/3">
           {posts.map((post) => (
