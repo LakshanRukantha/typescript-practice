a5b9b6800388510b895fe2c2a0e5c942ba06b3a6
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-10-23 20:40:31 +0530

    fix: Minor UI/UX Improvements

--- src/components/NewArticleBtn.tsx
+++ src/components/NewArticleBtn.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { HiOutlinePencilAlt } from "react-icons/hi";
+import {FaFeatherAlt} from "react-icons/fa";
 
 type NewArticleBtnProps = {
   url: string;
@@ -14,7 +14,7 @@ const NewArticleBtn = (props: NewArticleBtnProps) => {
       className="flex flex-col items-center justify-center p-2 border-2 hover:bg-violet-100 transition-all bg-gradient-to-tr hover:shadow-md from-violet-100 rounded-md hover:border-violet-500"
     >
       <div className="flex flex-row items-center gap-3">
-        <HiOutlinePencilAlt className="text-5xl text-violet-500" />
+        <FaFeatherAlt className="text-2xl md:text-3xl text-violet-500" />
         <h2 className="text-lg font-semibold text-slate-800">
           Write New Article
         </h2>
