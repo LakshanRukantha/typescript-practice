710ab66f4131b0f9ad338c137467db5ce09c921f
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-10-25 11:25:24 +0530

    fix: Minor UI/UX Improvements

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -3,7 +3,7 @@ import { ImSpinner8 } from "react-icons/im";
 const LoadingScreen = () => {
   return (
     <div className="flex items-center justify-center gap-2 md:gap-3 min-h-container-with-footer">
-      <ImSpinner8 className="animate-spin text-violet-500 h-6 w-6 md:h-8 md:w-8" />
+      <ImSpinner8 className="animate-spin text-violet-500 h-6 w-6" />
       <span className="animate-pulse">Loading Please Wait...</span>
     </div>
   );
